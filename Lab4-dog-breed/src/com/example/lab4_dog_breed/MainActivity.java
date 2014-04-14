a6b22ccbce2ae4b39cba681d935979100b12888f
NikolayM <6795662+in7l@users.noreply.github.com>
2014-04-14 16:22:49 +0300

    Finish Lab4-dog-breed.

--- Lab4-dog-breed/src/com/example/lab4_dog_breed/MainActivity.java
+++ Lab4-dog-breed/src/com/example/lab4_dog_breed/MainActivity.java
@@ -6,28 +6,33 @@ import android.app.ListActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends ListActivity {
-	private String[] dogs = { "test1", "test2" };
-	private int[] dogImageIds;
+	private String[] dogs;
+	private int[] dogImageIds = {  R.drawable.border_terrier, R.drawable.alaskan_malamute,
+			R.drawable.finnish_spitz, R.drawable.portuguese_water_dog, R.drawable.rottweiler,
+			R.drawable.beagle, R.drawable.greyhound, R.drawable.dachshund};
 	private TextView resultsTextView;
+	private ImageView resultsImageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		// Load the array list of dog breeds and their images.
-		//dogs = getResources().getStringArray(R.array.dogs);
-		//dogImageIds = getResources().getIntArray(R.array.dogImageIds);
+		// Load the array list of dog breeds.
+		dogs = getResources().getStringArray(R.array.dogs);
 		
-		// Load the textview for displaying results.
+		// Get the TextView and ImageView for displaying the selected options.
 		resultsTextView = (TextView) findViewById(R.id.Text1);
+		resultsImageView = (ImageView) findViewById(R.id.Image1);
 		
-		ArrayAdapter<String> myList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-				R.id.tv, dogs);
+		ArrayAdapter<String> myList = new ArrayAdapter<String>(this, R.layout.listitem,
+				 R.id.tv, dogs);
+		setListAdapter(myList);
 	}
 
 	@Override
@@ -39,7 +44,10 @@ public class MainActivity extends ListActivity {
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// Update the text selection
 		resultsTextView.setText("Selection: " + dogs[position]);
+		// Update the image.
+		resultsImageView.setImageResource(dogImageIds[position]);
 	}
 	
 	
