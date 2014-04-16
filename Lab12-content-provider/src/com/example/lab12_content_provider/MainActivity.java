a6af471bd7772786e69256c9725baf3d0609c76b
NikolayM <6795662+in7l@users.noreply.github.com>
2014-04-16 14:09:50 +0300

    Finish lab12-content-provider.

--- Lab12-content-provider/src/com/example/lab12_content_provider/MainActivity.java
+++ Lab12-content-provider/src/com/example/lab12_content_provider/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.lab12_content_provider;
 
+import java.util.ArrayList;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.app.Activity;
+import android.database.Cursor;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +17,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		displayContacts();
 	}
 
 	@Override
@@ -19,4 +28,50 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	private void displayContacts() {
+		TextView contactsTextView = (TextView)findViewById(R.id.ContactsTextView);
+		StringBuilder contactStringBuilder = new StringBuilder("Contacts:\n");
+		
+		Uri uri = ContactsContract.Contacts.CONTENT_URI;
+		Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+		while (cursor.moveToNext()) {
+			String contactId = cursor.getString(
+					cursor.getColumnIndex(ContactsContract.Contacts._ID));
+			String name = cursor.getString(
+					cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+			contactStringBuilder.append("Name: ").append(name);
+			
+			// Browse through the emails
+			Cursor emailCursor = getContentResolver().query(
+					ContactsContract.Data.CONTENT_URI,
+					null,
+					ContactsContract.Data.CONTACT_ID + "=" + contactId
+					+ " AND "
+					+ ContactsContract.Data.MIMETYPE + "='"
+					+ Email.CONTENT_ITEM_TYPE + "'",
+					null,
+					null);
+			
+			ArrayList<String> emails = new ArrayList<String>();
+			while (emailCursor.moveToNext()) {
+				emails.add(emailCursor.getString(
+						emailCursor.getColumnIndex(ContactsContract.Data.DATA1)));
+			}
+			emailCursor.close();
+			
+			if (emails.size() == 0) {
+				contactStringBuilder.append(" No emails");
+			}
+			else {
+				for (String email : emails) {
+					contactStringBuilder.append(" email: ").append(email);
+				}
+			}
+			contactStringBuilder.append("\n");
+		}
+		cursor.close();
+		
+		contactsTextView.setText(contactStringBuilder.toString());
+	}
 }
