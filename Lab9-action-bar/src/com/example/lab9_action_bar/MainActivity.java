dc23722738ff443c9fc5f7226fd55336af4801db
NikolayM <6795662+in7l@users.noreply.github.com>
2014-04-16 09:57:51 +0300

    Finish Lab9-action-bar.

--- Lab9-action-bar/src/com/example/lab9_action_bar/MainActivity.java
+++ Lab9-action-bar/src/com/example/lab9_action_bar/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.lab9_action_bar;
 
 import android.os.Bundle;
+import android.R.anim;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -11,42 +14,52 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private Toast toast;
-	private OnClickListener buttonOnClickListener = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			Button button = (Button)v;
-			// Get the text of the clicked button.
-			CharSequence text = button.getText();
-			// Set the text to the toast object.
-			toast.setText(text);
-			// Show the toast.
-			toast.show();
-		}
-	};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // Create a sinle Toast object that will display different messages.
+        // Create a single Toast object that will display different messages.
         Context context = getApplicationContext();
 		int duration = Toast.LENGTH_LONG;
 		toast = Toast.makeText(context, "", duration);
 		
-		// Register listeners with the buttons.
-		Button b1 = (Button)findViewById(R.id.ButtonImprison);
-		Button b2 = (Button)findViewById(R.id.ButtonRelease);
-		b1.setOnClickListener(buttonOnClickListener);
-		b2.setOnClickListener(buttonOnClickListener);
+		// Enable home button.
+		ActionBar bar = getActionBar();
+		bar.setHomeButtonEnabled(true);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.mymenu, menu);
         return true;
     }
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		String toastMessage = null;
+		switch (item.getItemId()) {
+			case R.id.ReleaseItem:
+				toastMessage = getString(R.string.release);
+				break;
+			case R.id.ImprisonItem:
+				toastMessage = getString(R.string.imprison);
+				break;
+			case android.R.id.home:
+				toastMessage = "Home button pressed";
+		}
+		
+		if (toastMessage != null) {
+			toast.setText(toastMessage);
+			toast.show();
+		}
+		
+		return true;
+	}
+    
     
 }
